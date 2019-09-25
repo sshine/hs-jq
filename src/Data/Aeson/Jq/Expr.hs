a13afcc2930a58c32279110f7a18a19be970ecaf
Simon Shine <shreddedglory@gmail.com>
2019-09-25 17:27:29 +0200

    Rewrite AST so it's syntax-faithful [#6]
    The AST is completely revised so that it models syntactic constructs
    in verbatim. This means that '.foo' and '."foo"', and so on, have
    different representations.
    
    Included:
     - Binary expression operators
     - FuncDef and Params
     - ObjKey to model difference between {foo: 42} and {"foo": 42}

--- src/Data/Aeson/Jq/Expr.hs
+++ src/Data/Aeson/Jq/Expr.hs
@@ -1,10 +1,5 @@
 
-module Data.Aeson.Jq.Expr
-  ( Expr(..)
-  , Term(..)
-  , ObjElem(..)
-  , Pattern(..)
-  ) where
+module Data.Aeson.Jq.Expr where
 
 import Data.Scientific (Scientific)
 import Data.Text (Text)
@@ -26,6 +21,12 @@ $ jq '.[.bar]' <<< '{ "foo": 42, "bar": "foo" }'
 $ jq '.foo[.bar]' <<< '{ "foo": [3,6,9], "bar": 2 }'
 9
 
+Note: 'as' is only available in subsequent expressions:
+
+$ jq -n '[ 1 as $answer | 2 ] | $answer' # fails
+
+TODO: Catch this in type-checking phase.
+
 Not addressed yet:
 
 $ jq '.foo[.bar + 1]' <<< '{ "foo": [3,6,9], "bar": 0 }' # arithmetic
@@ -38,42 +39,86 @@ $ jq '.foo.bar'  <<< '{ "foo": { "bar": 42 } }' # syntax sugar for '.foo | .bar'
 
 type Ident = Text
 
-data Expr = Term !Term
-          | Pipe !Expr !Expr
-          | As !Pattern
-          | Obj ![ObjElem]
-          | List ![Expr]
-          | StrLit !Text
-          | NumLit !Scientific
-          | BoolLit !Bool
-          | NullLit
-          deriving (Eq, Show)
+data Expr
+      -- Function definitions
+    = FuncDef !Ident !(Maybe Params) Expr
 
-data Term = Identity   -- .
-          | Index !Expr -- .foo, .["foo"], .[1], .[.foo]
-          | Var Ident
-          deriving (Eq, Show)
+      -- Control structures
+    | As !Expr !Pattern
+    | Reduce !Expr !Pattern !Expr !Expr
+    | Foreach !Expr !Pattern !Expr !Expr !(Maybe Expr)
+    | If ![(Expr, Expr)] !Expr
+    | TryCatch !Expr !(Maybe Expr)
+    | Label !Ident
+    | Break Ident                    -- 'break' $ IDENT (JBOL Term)
 
--- This is what JBOL's BNF calls MkDictPair.
---
--- I don't quite understand this part of the grammar:
---   Keyword ':' ExpD
---
--- Explanation:
---
--- jq supports {foo: "bar"} as shorthand for {"foo": "bar"}, and this
--- naturally extends to keywords. So the Keyword part of the BNF makes this
--- explicit.
---
--- The grammar only allows a subset of expressions, ExpD, to avoid ambiguity
--- in at least ':' and ',': Both of these can occur in expressions, but they
--- also occur as part of dictionary syntax.
---
--- For now, simplify things and deal with ambiguities in the parser.
-data ObjElem = ObjPair Expr (Maybe Expr)
-             deriving (Eq, Show)
+      -- Operators
+    | Assign !Expr !Expr             -- '='
+    | Or !Expr !Expr                 -- 'or'
+    | And !Expr !Expr                -- 'and'
+    | Alternative !Expr !Expr        -- '//'
+    | AlternativeAssign !Expr !Expr  -- '//='
+    | UpdateAssign !Expr !Expr       -- '|='
+    | Pipe !Expr !Expr               -- '|'
+    | Comma !Expr !Expr              -- ','
+    | Plus !Expr !Expr               -- '+'
+    | PlusAssign !Expr !Expr         -- '+='
+    | Minus !Expr !Expr              -- '-'
+    | MinusAssign !Expr !Expr        -- '-='
+    | Mult !Expr !Expr               -- '*'
+    | MultAssign !Expr !Expr         -- '*='
+    | Div !Expr !Expr                -- '/'
+    | DivAssign !Expr !Expr          -- '/='
+    | Mod !Expr !Expr                -- '%'
+    | ModAssign !Expr !Expr          -- '%='
+    | Eq !Expr !Expr                 -- '=='
+    | Neq !Expr !Expr                -- '!='
+    | Lt !Expr !Expr                 -- '<'
+    | Gt !Expr !Expr                 -- '>'
+    | Leq !Expr !Expr                -- '<='
+    | Geq !Expr !Expr                -- '>='
 
--- TODO: Split this into ObjExprPair, ObjIdentPair, singles, etc.
+      -- Terms
+
+      -- Prefix/postfix
+    | Optional !Expr                 -- suffix '?'
+    | Neg !Expr                      -- unary '-'
+    | DotFieldAfter !Expr !Ident     -- suffix '.foo'
+    | DotStrAfter !Expr !Text        -- suffix '."foo"'
+ -- | DotExp !Exp !Exp               -- suffix '.[e]' not supported by jq,
+                                     --   e.g. jq -n '{"foo":42}.["foo"]'
+      -- Begins with '.'
+    | Identity                       -- '.'
+    | RecursiveDescent               -- '..'
+    | DotField !Ident                -- '.foo'
+    | DotStr !Text                   -- '."foo"'
+    | DotExpr !Expr                  -- '.[e]'
+
+      -- Postfix indexing with []
+    | IndexAfter !(Maybe Expr) !Expr -- 'x[y]'
+    | IndexRangeAfter !Expr !(Maybe Expr) !(Maybe Expr)
+                                     -- postfix 'e[x:y]', 'e[x:]', 'e[:y]', 'e[:]'
+
+      -- Literals, variables
+    | Var Ident                      -- '$var' ([a-zA-Z_][a-zA-Z_0-9]*::)*[a-zA-Z_][a-zA-Z_0-9]*
+    | Obj ![(ObjKey, Maybe Expr)]      -- This is what JBOL's grammar calls MkDictPair
+    | List ![Expr]
+    | StrLit !Text
+    | NumLit !Scientific
+    | BoolLit !Bool
+    | NullLit
+
+      -- Explicit parentheses
+    | Paren !Expr
+    deriving (Eq, Show)
+
+data ObjKey = FieldKey !Ident
+            | FieldExpr !Expr
+            deriving (Eq, Show)
+
+data Params = IdentParam !Ident      -- '$' ident
+            | FilterParam !Ident     -- e.g. 'def foo(f): f | f'
+            deriving (Show, Eq)
 
 data Pattern = Ident !Text -- '... as $var'
              deriving (Eq, Show)
