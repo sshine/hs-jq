93165960c41c4f0e92eacb44eb4a13c5bb2ca222
Simon Shine <shreddedglory@gmail.com>
2019-09-25 17:40:33 +0200

    Support more of the AST in parser [#6]

--- src/Data/Aeson/Jq/Parser.hs
+++ src/Data/Aeson/Jq/Parser.hs
@@ -22,65 +22,116 @@ import           Data.Aeson.Jq.Expr
 
 type Parser = Parsec Void Text
 
--- TODO: Split expr in two again, one with and one without ':' and ','.
-
--- The split below is just to handle binary operators.
-
 expr :: Parser Expr
-expr = makeExprParser expr1 exprOperators
+expr = asum [ funcDef, exprOp ]
 
-exprOperators :: [[Operator Parser Expr]]
-exprOperators =
-  [ -- [ Prefix (Neg <$ sym "-") ]
-    [ InfixR (Pipe <$ sym "|") ]
+-- TODO: Find out if higher up means lower precedence (assumption now)
+-- TODO: Find out where to place try-catch in parser.
+exprOp :: Parser Expr
+exprOp = makeExprParser term
+  [ [ InfixR (Pipe              <$ sym "|") ]
+  , [ InfixL (Comma             <$ sym ",") ]
+  , [ InfixR (Alternative       <$ sym "//") ]
+  , [ InfixN (Assign            <$ sym "=")
+    , InfixN (UpdateAssign      <$ sym "|=")
+    , InfixN (PlusAssign        <$ sym "+=")
+    , InfixN (MinusAssign       <$ sym "-=")
+    , InfixN (MultAssign        <$ sym "*=")
+    , InfixN (DivAssign         <$ sym "/=")
+    , InfixN (ModAssign         <$ sym "%=")
+    , InfixN (AlternativeAssign <$ sym "//=")
+    ]
+  , [ InfixL (Or <$ sym "or") ]
+  , [ InfixL (And <$ sym "and") ]
+  , [ InfixN (Eq <$ sym "==")
+    , InfixN (Neq <$ sym "!=")
+    , InfixN (Leq <$ sym "<=")
+    , InfixN (Geq <$ sym ">=")
+    , InfixN (Lt <$ sym "<")
+    , InfixN (Gt <$ sym ">")
+    ]
+  , [ InfixL (Plus <$ sym "+")
+    , InfixL (Minus <$ sym "-")
+    ]
+  , [ InfixL (Mult <$ sym "*")
+    , InfixL (Div <$ sym "/")
+    , InfixL (Mod <$ sym "%")
+    ]
   ]
 
-expr1 :: Parser Expr
-expr1 = asum
-  [ Term <$> term
-  , Obj <$> obj
-  , List <$> list
-  , StrLit <$> string
-  , NumLit <$> number
+-- TODO: Find out if ':' and ',' actually have higher precedence than all others??
+--
+-- If not, this part of the parser needs to be restructured in some way where the
+-- operator overlap between ExpH (all expressions) and Exp (expressions that can
+-- go inside a dictionary literal, i.e. what JBOL grammar calls ExpD.)
+
+-- Note: The purpose of 'Term' vs. 'Exp' seems to be to separate the binary operators
+-- (CFG non-terminals) from the unary operators (CFG terminals). This distinction is
+-- only preserved in the parser combinator names; the AST has just Exp (and ExpH).
+term :: Parser Expr
+term = asum
+  [ Obj     <$> obj
+  , List    <$> list
+  , StrLit  <$> string
+  , NumLit  <$> number
   , BoolLit <$> bool
   , NullLit <$ sym "null"
-  , between (sym "(") (sym ")") expr -- TODO: Make AST constructor
+  , Paren   <$> parens expr
+  , dotExpr
+  , Var     <$> var
   ]
 
-term :: Parser Term
-term = asum
-  [ dotIndexTerm
-  , Var <$> var
-  ]
-
-dotIndexTerm :: Parser Term
-dotIndexTerm = do
+dotExpr :: Parser Expr
+dotExpr = do
   dot
-  asum [ Index <$> brackets expr
-       , Index . StrLit <$> lexeme field
+  asum [ DotExpr          <$> brackets expr
+       , DotStr           <$> string
+       , DotField         <$> lexeme field
+       , RecursiveDescent <$ dot
+       , pure Identity
        ]
 
--- TODO: Parse keywords.
--- TODO: Make another 'field' parser that disallows keywords
--- TODO: Rename 'field :: Parser Text' to 'ident :: Parser Ident'
+funcDef :: Parser Expr
+funcDef =
+  sym "def" $> FuncDef
+    <*> lexeme field
+    <*> optional params
+    <*> between (sym ":") (sym ";") expr
+
+params :: Parser Params
+params = parens . asum $
+  []
+
+field :: Parser Text
+field = fieldK >>= notKeyword
+  where
+    notKeyword word = if word `elem` keywords
+      then fail ("Keyword " ++ show word ++ " cannot be used as identifier")
+      else return word
+
+    keywords = [ "module", "import", "include", "def", "as"
+               , "if", "then", "else", "elif", "end", "and"
+               , "or", "reduce", "foreach", "try", "catch"
+               , "label", "break", "__loc__"
+               ]
 
 -- [a-zA-Z_][a-zA-Z_0-9]*
-field :: Parser Text
-field = label "field" $
+fieldK :: Parser Text
+fieldK = label "field" $
   Text.cons <$> satisfy isFieldFirstChar
             <*> takeWhile1P Nothing isFieldChar
   where
     isFieldFirstChar c = isAscii c && isLetter c || c == '_'
     isFieldChar c = isFieldFirstChar c || isDigit c
 
-obj :: Parser [ObjElem]
-obj = between (sym "{") (sym "}") (objElem `sepBy` sym ",")
+obj :: Parser [(ObjKey, Maybe Expr)]
+obj = objElem `sepBy` sym ","
 
--- TODO: This is way too liberal. See notes in AST.
-objElem :: Parser ObjElem
-objElem = ObjPair <$> key <*> value
+objElem :: Parser (ObjKey, Maybe Expr)
+objElem = (,) <$> key <*> value
   where
-    key = (StrLit <$> field) <|> expr
+    key = asum [ FieldKey <$> fieldK
+               , FieldExpr <$> expr ] -- FIXME: Too permissive.
     value = optional (sym ":" *> expr)
 
 list :: Parser [Expr]
@@ -90,9 +141,8 @@ var :: Parser Text
 var = chunk "$" *> field
 
 string :: Parser Text
-string = quotes content
+string = between (sym "\"") (sym "\"") content
   where
-    quotes = between (sym "\"") (sym "\"")
     content = takeWhileP Nothing (\c -> c /= '"' && c /= '\\')
     -- FIXME: Handle escape sequences in strings. It may have made sense to
     -- use Aeson's parser for this, but it's written in Attoparsec. Also,
@@ -131,6 +181,9 @@ bool = asum [ sym "true" $> True
 
 dot :: Parser ()
 dot = sym "." <?> "dot"
+
+parens :: Parser a -> Parser a
+parens = between (sym "(") (sym ")")
 
 brackets :: Parser a -> Parser a
 brackets = between (sym "[") (sym "]")
